7643a75d4a4c441a8defad36d612fcde839e2ea0
gseyoum <getasey@gmail.com>
2022-04-15 10:04:43 -0400

    save

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -1,29 +1,23 @@
 let slidePosition = 0;
-const slides = document.querySelectorAll('.photo-grid-item');
-const totalslides = slides.length;
+const slides = document.querySelectorAll('carousel_item');
+const totalSlides = slides.length;
 
-document.querySelector('#carousel_button--next')
-.addEventListener("click",function(){
-    movetonextslide();
+document
+  .querySelector('#carousel_button--next')
+  .addEventListener("click", function() {
+        movetonextslide();
     console.log('test');
-});
-
-document.querySelector('#carousel_button--prev')
-.addEventListener("click",function(){
-    moveToPrevSlide();
-    console.log('test');
-});
-
-function updateSlidePostion(){
-    for(let slide of slides){
-        slide.classList.remove('photo-visible');
-        slide.classList.add('photo-grid-item--hidden')
-    }
-    slides[slidePosition].classList.add('photo-visible');
-    slides[slidePosition].classList.remove('photo-grid-item--hidden');
-    console.log(slides);
-    console.log(slidePosition);
-}
+    });
+document.
+    querySelector('#carousel_button--prev')
+    .addEventListener("click",function(){
+        moveToPrevSlide();
+    });
+    function updateSlidePostion() {
+        for(let slide of slides){
+            slide.classList.remove('photo-visible');
+            slide.classList.add('photo-grid-item--hidden')
+    }}
 
 
 function moveToNextSlide(){
