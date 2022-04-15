319e2e33866aa2dbef792b8a50cd9eab70f0584b
gseyoum <getasey@gmail.com>
2022-04-15 10:08:43 -0400

    save

--- client/lab_4/script.js
+++ client/lab_4/script.js
@@ -2,34 +2,36 @@ let slidePosition = 0;
 const slides = document.querySelectorAll('carousel_item');
 const totalSlides = slides.length;
 
-document
-  .querySelector('#carousel_button--next')
-  .addEventListener("click", function() {
+document.
+    querySelector('#carousel_button--next')
+    .addEventListener("click", function() {
         movetonextslide();
-    console.log('test');
     });
 document.
-    querySelector('#carousel_button--prev')
-    .addEventListener("click",function(){
+    querySelector('carousel_button--prev')
+    .addEventListener("click", function() {
         moveToPrevSlide();
     });
     function updateSlidePostion() {
-        for(let slide of slides){
-            slide.classList.remove('photo-visible');
-            slide.classList.add('photo-grid-item--hidden')
-    }}
+        for(let slide of slides) {
+            slide.classList.remove('carusel_item--visable');
+            slide.classList.add('photo-grid-item--hidden');
+        }
+        slides[slidePosition].classList.add('carousel_item--visale');
+    }
 
 
-function moveToNextSlide(){
-    if(slidePosition === totalSlides - 1){
+function moveToNextSlide(){ 
+    updateSlidePostion();
+
+    if(slidePosition === totalSlides - 1) {
         slidePosition = 0;
     } else{
         slidePosition++;
     }
-    updateSlidePostion()
 }
 
-function moveToPrevSlide(){
+function moveToPrevSlide() {
     if(slidePosition === 0){
         slidePosition = totalSlides - 1;
     } else{
