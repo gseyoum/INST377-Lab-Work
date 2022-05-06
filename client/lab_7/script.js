8f22b081f0014a6de6bc494762aa803d1c5f43b8
gseyoum <getasey@gmail.com>
2022-05-06 16:58:41 -0400

    7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -1,95 +1,81 @@
-async function setup() {
-  const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
-  const request = await fetch(endpoint);
-  const data = await request.json();
-  const table = document.querySelector('#result-table');
-  const tableResults = document.querySelector('#result-table-results');
-  const noResults = document.querySelector('#no-results');
-  const searchForm = document.querySelector('#search-form');
-  const searchTerm = document.querySelector('#search-term');
-  const map = L.map('result-map');
-  let markers = [];
-
-  function findMatches(e, data = []) {
-    if (searchTerm.value.length <= 2) {
-      buildResultUI();
-      return;
-    }
-    const query = searchTerm.value.toLowerCase();
-    const basis = document.querySelector('input[name="search_type"]:checked').value;
-    const results = [];
-    data.forEach((d) => {
-      if (basis === 'zip' && d.zip.includes(query)) {
-        results.push(d);
-      }
-    });
-    buildResultUI(results);
-  }
-
-  function buildResultUI(results = []) {
-    if (!results || !(results instanceof Array) || results.length <= 0) {
-      noResults.classList.remove('is-hidden');
-      table.classList.add('is-hidden');
-    } else {
-      noResults.classList.add('is-hidden');
-      table.classList.remove('is-hidden');
-    }
-    const term = searchTerm.value;
-    const regex = new RegExp(term, 'gi');
-    const fragment = document.createDocumentFragment();
-    const coords = [];
-    (results || []).splice(0, 4).forEach((restaurant) => {
-      const tr = document.createElement('tr');
-      tr.innerHTML = `<td>${restaurant.name.toUpperCase()}</td><td>${restaurant.address_line_1}</td><td>${restaurant.city}, ${restaurant.state}</td>`
-        .replace(regex, `<b class='has-background-info'>${term.toUpperCase()}</b>`);
-
-      // Appending tr
-      fragment.appendChild(tr);
-      coords.push([restaurant.name.toUpperCase(), restaurant.geocoded_column_1.coordinates]);
-    });
-
-    // Appending fragment
-    tableResults.innerHTML = '';
-    tableResults.appendChild(fragment);
-
-    buildMarkers(coords);
-  }
-
-  function buildMarkers(locations = []) {
-    markers.forEach((m) => map.removeLayer(m));
-    markers = [];
-    if (!locations || locations.length <= 0) {
-      map.setView([38.83986, -76.941642], 5);
-      return;
-    }
-    locations.forEach((loc) => {
-      markers.push(new L.Marker([loc[1][1], loc[1][0]], {draggable: false}).bindPopup(loc[0]).openPopup());
-    });
-    markers.forEach((m) => map.addLayer(m));
-    map.setView([locations[0][1][1], locations[0][1][0]], 12);
-  }
-
-  searchForm.onsubmit = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    findMatches(e, data);
-  };
-  searchTerm.onsubmit = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    findMatches(e, data);
-  };
-  searchTerm.onkeyup = (e) => findMatches(e, data);
-
-  // Using api for map location
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: '',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiYW1hdHR1IiwiYSI6ImNrdWw1eGxheTNldGUydXFsbjBpcm52M28ifQ.vm917QE5p4Dk7wvHRRLwUw'
-  }).addTo(map);
-  map.setView([38.83986, -76.941642], 5);
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(
+    Math.random() * (newMax - newMin + 1) + newMin
+  );
 }
-window.onload = (e) => setup();
+
+function restoArrayMake(dataArray) {
+  // console.log('fired dataHandler');
+  // console.table(dataArray);
+  const range = [...Array(15).keys()];
+  const listItems = range.map((item, index) => {
+    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+
+    return dataArray[restNum];
+  });
+    //  console.log(listItems);
+  return listItems;
+}
+function createHtmlList(collection) {
+  //  console.log('fired HTML creator');
+  //  console.log(collection);
+  const targetList = document.querySelector('.resto-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThisItem = `<li>${displayName}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+// As the last step of your lab, hook this up to index.html
+async function mainEvent() { // the async keyword means we can make API requests
+  console.log('script loaded');
+  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
+  const submit = document.querySelector('.submit_button');
+
+  const resto = document.querySelector('#resto_name');
+  const zipcode = document.querySelector('#zipcode');
+  submit.style.display = 'none';
+
+  const results = await fetch('/api/foodServicesPG');
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
+  // console.log(arrayFromJson);
+
+  if (arrayFromJson.data.length > 0) {
+    submit.style.display = 'block';
+
+    let currentArray = [];
+    resto.addEventListener('input', async(event) => {
+      console.log(event.target.value);
+
+      if (currentArray.length < 1) {
+        return;
+      }
+
+      const selectResto = currentArray.filter((item) => {
+        const lowerName = item.name.toLowerCase();
+        const lowerValue = event.target.value.toLowerCase();
+        return lowerName.includes(lowerValue);
+      });
+
+      console.log(selectResto);
+      createHtmlList(selectResto);
+    });
+
+    form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
+      submitEvent.preventDefault(); // This prevents your page from refreshing!
+      //  console.log('form submission'); // this is substituting for a "breakpoint"
+
+      currentArray = restoArrayMake(arrayFromJson.data);
+      console.log(currentArray);
+      createHtmlList(currentArray);
+    });
+  }
+}
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
