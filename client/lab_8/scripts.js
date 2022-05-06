3db52b76bfc8d7d80057a132ede4a2a105516f1d
gseyoum <getasey@gmail.com>
2022-05-06 17:25:25 -0400

    8

---
+++ client/lab_8/scripts.js
@@ -0,0 +1,117 @@
+function getRandomIntInclusive(min, max) {
+    const newMin = Math.ceil(min);
+    const newMax = Math.floor(max);
+    return Math.floor(
+      Math.random() * (newMax - newMin + 1) + newMin
+    );
+  }
+  
+  function restoArrayMake(dataArray) {
+    // console.log('fired dataHandler');
+    // console.table(dataArray);
+    const range = [...Array(15).keys()];
+    const listItems = range.map((item, index) => {
+      const restNum = getRandomIntInclusive(0, dataArray.length - 1);
+  
+      return dataArray[restNum];
+    });
+    // console.log(listItems);
+    return listItems;
+  }
+  function createHtmlList(collection) {
+    // console.log('fired HTML creator');
+    // console.log(collection);
+    const targetList = document.querySelector('.resto-list');
+    targetList.innerHTML = '';
+    collection.forEach((item) => {
+      const {name} = item;
+      const displayName = name.toLowerCase();
+      const injectThisItem = `<li>${displayName}</li>`;
+      targetList.innerHTML += injectThisItem;
+    });
+  }
+  
+  function initMap(targetId) {
+    const latLong = [38.7849, -76.8721];
+    const map = L.map(targetId).setView(latLong, 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
+    }).addTo(map);
+    return map;
+  }
+  
+  function addMapMarkers(map, collection) {
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker) {
+        layer.remove();
+      }
+    });
+  
+    collection.forEach((item) => {
+      const point = item.geocoded_column_1?.coordinates;
+      console.log(item.geocoded_column_1?.coordinates);
+      L.marker([point[1], point[0]]).addTo(map);
+    });
+  }
+  
+  async function mainEvent() {
+    console.log('script loaded');
+    const form = document.querySelector('.main_form');
+    const submit = document.querySelector('.submit_button');
+  
+    const resto = document.querySelector('#resto_name');
+    const zipcode = document.querySelector('#zipcode');
+    const map = initMap('map');
+    const retrievalVar = 'restaurants';
+    submit.style.display = 'none';
+  
+    if (!localStorage.getItem(retrievalVar)) {
+      const results = await fetch('/api/foodServicesPG');
+      const arrayFromJson = await results.json();
+      console.log(arrayFromJson);
+      localStorage.setItem(retrievalVar, JSON.stringify(arrayFromJson.data));
+    }
+    const storedDataString = localStorage.getItem(retrievalVar);
+    const storedDataArray = JSON.parse(storedDataString);
+    console.log(storedDataArray);
+    // const arrayFromJson = {data: []};
+  
+    if (storedDataArray.length > 0) {
+      submit.style.display = 'block';
+  
+      let currentArray = [];
+      resto.addEventListener('input', async(event) => {
+        console.log(event.target.value);
+  
+        if (currentArray.length < 1) {
+          return;
+        }
+  
+        const selectResto = storedDataArray.filter((item) => {
+          const lowerName = item.name.toLowerCase();
+          const lowerValue = event.target.value.toLowerCase();
+          return lowerName.includes(lowerValue);
+        });
+  
+        console.log(selectResto);
+        createHtmlList(selectResto);
+      });
+  
+      form.addEventListener('submit', async (submitEvent) => {
+        submitEvent.preventDefault();
+        // console.log('form submission'); // this is substituting for a "breakpoint"
+  
+        currentArray = restoArrayMake(storedDataArray);
+        console.log(currentArray);
+        createHtmlList(currentArray);
+        addMapMarkers(map, currentArray);
+      });
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
