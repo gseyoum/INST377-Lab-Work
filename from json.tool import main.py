3cdbfdcbb6928a3b9d32ed0eb06e75ab8349234d
gseyoum <getasey@gmail.com>
2022-05-05 22:01:46 -0400

    save

---
+++ from json.tool import main.py
@@ -0,0 +1,14 @@
+from json.tool import main
+from random import randint
+TEAMS = ["Nice","Marseille","Paris","Saint-Etienne","Lille","Lyon","Bordeaux","Lens","Rennes","Troyes"]
+
+class League:
+    def __init__(self,teams) -> None:
+        pass
+    results=()
+
+    def simulate(self):
+        self.randint.random(1-10)
+        
+if __name__ == __main__:
+    print(team_record)
